f9d564b9ca258a59fc7cb14a613dc372b011f37f
trongnghia2304 <nghiatrongnguyen2304@gmail.com>
2024-06-14 23:39:47 +0700

    add boilerplate

--- seeds/index.js
+++ seeds/index.js
@@ -24,9 +24,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://source.unsplash.com/collection/483251`,
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis earum voluptate ipsa dolores autem perspiciatis sed, vel ducimus, commodi aspernatur laudantium perferendis tempora animi? Quae quaerat quidem nobis praesentium commodi.',
+            price
         })
         await camp.save();
     }
